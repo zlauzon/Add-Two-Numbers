ff7a506b5a0dbf7a9b3355c0569aeccb93c12c15
zlauzon <9244781+zlauzon@users.noreply.github.com>
2022-07-26 16:06:04 -0600

    document solution

--- add-two-numbers-solution.cpp
+++ add-two-numbers-solution.cpp
@@ -22,7 +22,11 @@ public:
         ListNode* temp;
         ListNode* tempPrevious;
         
+        //We need to assign and remember the head to our returned linked list so we process the
+        //first sum outside the loop.
         int sum = l1->val + l2->val;
+        //Only a single digit is stored in each node so we remember the additional digits and
+        //add it to the next sum.
         int carryOver = sum / 10;
         l1 = l1->next;
         l2 = l2->next;
@@ -30,6 +34,9 @@ public:
         ListNode* head = new ListNode(sum % 10);
         temp = head;
         tempPrevious = temp;
+        
+        //Check for carryOver because nodes may add up to a multi digit number that would create new nodes within
+        //the return beyond the lengths of the passed linked lists.
         while(l1 || l2 || carryOver){
             int lhs = 0;
             int rhs = 0;
@@ -47,7 +54,7 @@ public:
             carryOver = sum / 10;
             temp = new ListNode(sum % 10);
             if (temp != tempPrevious)
-                tempPrevious->next = temp;
+                tempPrevious->next = temp; //Ensure the previous node is pointed to the current node
             tempPrevious = tempPrevious->next;
                 
         }
